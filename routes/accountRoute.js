455bb9aae12624f406bfec8a930f958b49bca4bc
lsmills24 <97650683+lsmills24@users.noreply.github.com>
2025-02-20 21:28:43 -0700

    register, login, and validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,12 +3,20 @@ const express = require("express")
 const router = new express.Router()
 const utilities = require("../utilities/index")
 const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
 // Route to get login screen
 router.get("/login", utilities.handleErrors(accountController.buildLogin)); 
 // Route to build registration
 router.get("/register", utilities.handleErrors(accountController.buildRegister)); 
-// Route to post a new registration
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// Route to post a new registration & process account validation data
+router.post(
+    "/register", 
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+)
+
+
 
 
 module.exports = router;
